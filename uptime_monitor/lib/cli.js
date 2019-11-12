37795f205b3bb57762fa8e71ef649646b0b60f12
sagar_dev <sagarkukreja4@gmail.com>
2019-11-12 07:19:30 +0530

    CLI application done

--- uptime_monitor/lib/cli.js
+++ uptime_monitor/lib/cli.js
@@ -7,7 +7,12 @@
 var readLine = require('readline'),
     util = require('util'),
     debug = util.debuglog('cli'),
-    events = require('events');
+    events = require('events'),
+    os = require('os'),
+    v8 = require('v8'),
+    _data = require('./data'),
+    _log = require('./logs'),
+    helper = require('./helpers');
 
 class _events extends events{};
 var e = new _events();
@@ -76,7 +81,7 @@ e.on('user info', function(str){
 });
 
 e.on('list checks', function(str){
-    cli.responders.listChecks();
+    cli.responders.listChecks(str);
 });
 
 e.on('check info', function(str){
@@ -106,7 +111,7 @@ cli.responders.help = function(){
         'user info --{userId}': 'Show details of a specified user',
         'list checks --up --down': 'Show a list of all active checks in the system, including their state. The "--up" and the "--down" flags are both optional',
         'check info --{checkID}': 'Show details of a specified check',
-        'list logs': 'Show a list of all the log files (compressed and uncompressed) to be read',
+        'list logs': 'Show a list of all the log files (compressed only) to be read',
         'log info --{fileName}': 'Show details of a specified log file'
     };
 
@@ -142,42 +147,180 @@ cli.responders.exit = function(){
 
 // stats responder
 cli.responders.stats = function(){
-    console.log('You asked for stats');
+    // Compile stats object
+    var stats = {
+        'Load Average': os.loadavg().join(' '),
+        'CPU Count': os.cpus().length,
+        'Fre Memory': os.freemem(),
+        'Current Malloced Memory': v8.getHeapStatistics().malloced_memory,
+        'Peak Malloced Memory': v8.getHeapStatistics().peak_malloced_memory,
+        'Allocated Heap Used (%)': Math.round((v8.getHeapStatistics().used_heap_size / v8.getHeapStatistics().total_heap_size) * 100),
+        'Available Heap Allocated (%)': Math.round((v8.getHeapStatistics().total_heap_size / v8.getHeapStatistics().heap_size_limit) * 100),
+        'Uptime': os.uptime() + ' seconds'
+    };
+
+    // Create Header
+    cli.horizontalLine();
+    cli.centered('SYSTEM STATISTICS');
+    cli.horizontalLine();
+    cli.verticalSpace(2);
+
+    // Log out each stat
+    for(var key in stats){
+        if(stats.hasOwnProperty(key)){
+            var value = stats[key],
+                line = '\x1b[33m' + key + '\x1b[0m',
+                padding = 60 - line.length;
+
+            for(var i = 0; i < padding; i++){
+                line += ' ';
+            }
+            line += value;
+            console.log(line);
+            cli.verticalSpace();
+        }
+    }
+
+    // Footer
+    cli.verticalSpace(1);
+    cli.horizontalLine();
+
 };
 
 // users responder
 cli.responders.listUsers = function(){
-    console.log('You asked for users');
+    
+    _data.list('users', function(err, userIds){
+        if(!err && userIds && userIds.length > 0){
+            userIds.forEach(function(userId){
+                _data.read('users', userId, function(err, userData){
+                    if(!err && userData){
+                        var line = 'Name: '+userData.firstName+' '+userData.lastName+' Phone: '+userData.phone+' Checks: ';
+                        var numberOfChecks = typeof(userData.checks) == 'object' && userData.checks instanceof Array && userData.checks.length > 0 ? userData.checks.length : 0;
+
+                        line += numberOfChecks;
+
+                        console.log(line);
+                        cli.verticalSpace();
+                    }
+                });
+            });
+        }
+    })
 };
 
 // user-info responder
 cli.responders.userInfo = function(str){
-    console.log('You asked for user info', str);
+    // Get user-id from str
+    var arr = str.split('--');
+    var userId = typeof(arr[1]) == 'string' && arr[1].trim().length > 0 ? arr[1].trim() : false;
+
+    if(userId){
+        // Lookup for user
+        _data.read('users', userId, function(err, userData){
+            if(!err && userData){
+                // Remove hashed password
+                delete userData.hashedPassword;
+
+                // Print JSON with highlighting text
+                cli.verticalSpace();
+                console.dir(userData, {'colors': true});
+                cli.verticalSpace();
+            }
+        });
+    }
 };
 
 // checks responder
-cli.responders.listChecks = function(){
-    console.log('You asked for checks');
+cli.responders.listChecks = function(str){
+
+    _data.list('checks', function(err, checkIds){
+        if(!err && checkIds && checkIds.length > 0){
+
+            // cli.verticalSpace();
+            str = str.toLowerCase();
+
+            checkIds.forEach(function(checkId){
+                _data.read('checks', checkId, function(err, checkData){
+                    // Get state, default to down
+                    var state = typeof(checkData.state) == 'string' ? checkData.state : 'down';
+
+                    // Get the state, default to unknown (Display purpose)
+                    var stateOrUnknown = typeof(checkData.state) == 'string' ? checkData.state : 'unknown';
+
+                    // If user has specified the state or hasn't specified any state, incude the check
+                    if(str.indexOf('--'+state) > -1 || (str.indexOf('--up') == -1 && str.indexOf('--down') == -1)){
+                        var line = 'Id: '+checkData.checkId+' '+checkData.method.toUpperCase()+' '+checkData.protocol+'://'+checkData.url+' State: '+stateOrUnknown;
+
+                        console.log(line);
+                        cli.verticalSpace();
+                    } 
+                });
+            });
+        }
+    });
 };
 
 // check-info responder
 cli.responders.checkInfo = function(str){
-    console.log('You asked for check info', str);
+    // Get check-id from str
+    var arr = str.split('--');
+    var checkId = typeof(arr[1]) == 'string' && arr[1].trim().length > 0 ? arr[1].trim() : false;
+
+    if(checkId){
+        // Lookup for check
+        _data.read('checks', checkId, function(err, checkData){
+            if(!err && checkData){
+                // Print JSON with highlighting text
+                cli.verticalSpace();
+                console.dir(checkData, {'colors': true});
+                cli.verticalSpace();
+            }
+        });
+    }
 };
 
 // logs responder
 cli.responders.listLogs = function(){
-    console.log('You asked for logs');
+    _log.list(true, function(err, logFileNames){
+        if(!err && logFileNames && logFileNames.length > 0){
+            logFileNames.forEach(function(logFileName){
+                if(logFileName.indexOf('-') > -1){
+                    console.log(logFileName);
+                    cli.verticalSpace();
+                }
+            });
+        }
+    });
 };
 
 // log-info responder
 cli.responders.LogInfo = function(str){
-    console.log('You asked for log info', str);
+    // Get log file name from str
+    var arr = str.split('--');
+    var logFileName = typeof(arr[1]) == 'string' && arr[1].trim().length > 0 ? arr[1].trim() : false;
+
+    if(logFileName){
+        // Lookup for log file
+        _log.decompress(logFileName, function(err, strData){
+            if(!err && strData){
+                // Split data into lines
+                var arr = strData.split('\n');
+                arr.forEach(function(jsonString){
+                    var logObject = helper.parseJsonToObject(jsonString);
+                    if(logObject && JSON.stringify(jsonString) != '{}'){
+                        console.dir(logObject, {'colors': true});
+                        cli.verticalSpace();
+                    }
+                });
+            }
+        });
+    }
 };
 
 // Input processor
 cli.processInput = function(str){
-    str = typeof(str) == "string" && str.trim().length > 1 ? str : false;
+    str = typeof(str) == "string" && str.trim().length > 0 ? str : false;
 
     // Only process if user has wrote something
     if(str){
